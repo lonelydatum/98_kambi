04cb66880a500dd8d1753d0f554bc86586f49d8f
Gar <gar@lonelydatum.com>
2025-12-17 14:03:54 -0500

    on

--- dev/plusBettingEasier_970x250/_bundled/main.js
+++ dev/plusBettingEasier_970x250/_bundled/main.js
@@ -171,11 +171,43 @@ function olg() {
 exports.olg = olg;
 
 },{}],4:[function(require,module,exports){
-'use strict';
+"use strict";
 
 var _commonJsCommonJs = require('../../_common/js/common.js');
 
-(0, _commonJsCommonJs.init_728x90)();
+function init_728x90() {
+	var tl = new TimelineMax({ onComplete: function onComplete() {
+			if (document.getElementById("legalBtn")) {
+				TweenLite.set("#legalBtn", { display: "block" });
+			}
+		} });
+
+	tl.set(".frame1", { opacity: 1 });
+	tl.from(".arrows", { opacity: 0, duration: .3 });
+	tl.from(".phone_1a", { opacity: 0, y: "+=80", duration: .3 });
+	tl.from(".t1", { opacity: 0, duration: .3 });
+
+	tl.to([".t1", ".phone_1a"], { opacity: 0, duration: .3 }, "+=" + _commonJsCommonJs.read.t1);
+	tl.add("t2");
+	tl.from(".phone_1b", { opacity: 0, duration: .3 }, "t2");
+	tl.from(".t2", { opacity: 0, duration: .3 }, "t2+=1");
+
+	tl.from(".hero", { opacity: 0, duration: .5 }, "+=" + _commonJsCommonJs.read.t2);
+	tl.from(".txt_uyg", { opacity: 0, duration: .3 }, "+=.3");
+
+	tl.from(".arrow_hero", { opacity: 0, duration: .6 }, "+=" + 1.5);
+
+	tl.from(".phone_2", { opacity: 0, y: "+=80", duration: .3 });
+	tl.from(".txt_dta", { opacity: 0, y: "+=40", duration: .3 });
+
+	tl.from([".playsmart", ".legal"], { opacity: 0, duration: .3 });
+
+	tl.add((0, _commonJsCommonJs.olg)());
+
+	return tl;
+}
+
+init_728x90();
 
 },{"../../_common/js/common.js":1}]},{},[4])
 
